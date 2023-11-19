23457c6bd2bf4854ea372d59c283011232a424cc
Domnick Eger <deger@cobercafe.net>
2023-11-19 13:38:16 -0700

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,8 @@
+anjuna-nitro-cli terminate-enclave --all
+
+# Also terminate the network bridge between enclave and parent
+# in case there are changes to the exposed ports.
+pkill -f "anjuna-nitro-netd-parent"
+
+# Same for the filesystem proxy
+pkill -f "anjuna-fs-proxy"
